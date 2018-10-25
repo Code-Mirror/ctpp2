8a4db2c1e401703bab5217c1270c999647f7e6ed
RekGRpth <rekgrpth@gmail.com>
2018-10-25 10:45:49 +0500

    clean

--- ClearPackage.sh
+++ ClearPackage.sh
@@ -4,4 +4,4 @@ if [ -f Makefile ]; then
     make clean
 fi
 
-rm -fr CMakeCache.txt CMakeFiles DartTestfile.txt Makefile Testing cmake_install.cmake progress.make include/CTPP2SysHeaders.h doc/html *.ct2 install_manifest.txt build-stamp configure-stamp debian/ctpp2 debian/ctpp2-dev debian/tmp debian/ctpp2.postinst.debhelper debian/ctpp2.postrm.debhelper debian/ctpp2.substvars debian/dirs debian/files ctpp2-config CTestTestfile.cmake ctpp2-config *.out template_language.html
+rm -fr CMakeCache.txt CMakeFiles DartTestfile.txt Makefile Testing cmake_install.cmake progress.make include/CTPP2SysHeaders.h doc/html *.ct2 install_manifest.txt build-stamp configure-stamp debian/ctpp2 debian/ctpp2-dev debian/tmp debian/ctpp2.postinst.debhelper debian/ctpp2.postrm.debhelper debian/ctpp2.substvars debian/dirs debian/files ctpp2-config CTestTestfile.cmake ctpp2-config *.out template_language.html BitIndexTest CDT2JSONBenchmark CDTPerfTest CDTTest CTPP2DTOATest CTPP2VMTest HashTest StaticTextTest VMArgStackTest VMCodeStackTest ctpp2c ctpp2i ctpp2json ctpp2vm libctpp2-st.a simple-compiler simple-vm
